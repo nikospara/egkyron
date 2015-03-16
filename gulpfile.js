2e93ae2a4ec8b4faa164f75ac912e46aedd69197
Nikos Paraskevopoulos <nikospara@yahoo.com>
2015-03-17 00:59:33 +0200

    Added the ConstructorIntrospector and relevant gulpfile sections

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
-gulp.task('jshint', function() {
+gulp.task('core.jshint', function() {
 	var jshint = require('gulp-jshint');
 
 	return gulp
@@ -10,7 +10,7 @@ gulp.task('jshint', function() {
 		.pipe(jshint.reporter('fail'));
 });
 
-gulp.task('jsdoc', ['jshint'], function() {
+gulp.task('core.jsdoc', ['core.jshint'], function() {
 	var jsdoc = require('gulp-jsdoc');
 
 	return gulp
@@ -18,7 +18,7 @@ gulp.task('jsdoc', ['jshint'], function() {
 		.pipe(jsdoc('./target/docs/core'));
 });
 
-gulp.task('test', ['jshint', 'package'], function() {
+gulp.task('core.test', ['core.jshint', 'core.package'], function() {
 	var
 		jshint = require('gulp-jshint'),
 		jasmine = require('gulp-jasmine');
@@ -31,7 +31,7 @@ gulp.task('test', ['jshint', 'package'], function() {
 		.pipe(jasmine({includeStackTrace: true}));
 });
 
-gulp.task('package', function() {
+gulp.task('core.package.node', ['core.jshint'], function() {
 	var
 		tap = require('gulp-tap'),
 		affix = require('./src/build/gulp-affix.js');
@@ -40,4 +40,34 @@ gulp.task('package', function() {
 		.src('./src/core/main/**/*.js')
 		.pipe(tap(affix('./src/packaging/node/')))
 		.pipe(gulp.dest('./target/dist/core/node'));
+});
+
+
+gulp.task('constrat.jshint', function() {
+	var jshint = require('gulp-jshint');
+
+	return gulp
+		.src('./src/constraints-strategy/main/**/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(jshint.reporter('fail'));
+});
+
+gulp.task('constrat.jsdoc', ['constrat.jshint'], function() {
+	var jsdoc = require('gulp-jsdoc');
+
+	return gulp
+		.src('./src/constraints-strategy/main/**/*.js')
+		.pipe(jsdoc('./target/docs/constraints-strategy'));
+});
+
+gulp.task('constrat.package.node', ['constrat.jshint'], function() {
+	var
+		tap = require('gulp-tap'),
+		affix = require('./src/build/gulp-affix.js');
+
+	return gulp
+		.src('./src/constraints-strategy/main/**/*.js')
+		.pipe(tap(affix('./src/packaging/node/constraints-strategy/')))
+		.pipe(gulp.dest('./target/dist/core/node/constraints-strategy'));
 });
